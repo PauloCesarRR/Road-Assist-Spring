f470e8eab06b6233b1b5da4fec47b35aef09d133
PauloCesarRR <paulocesarrrpontes@gmail.com>
2023-10-17 18:03:59 -0300

    :tada: build project

---
+++ src/main/java/br/com/fiap/roadassist/RoadassistApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.roadassist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoadassistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoadassistApplication.class, args);
+	}
+
+}
