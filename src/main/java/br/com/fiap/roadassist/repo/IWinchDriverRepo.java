893039fe907dfd2c7ae534d67d0c3cf52c49d179
PauloCesarRR <paulocesarrrpontes@gmail.com>
2023-10-18 19:21:17 -0300

    fix: fixed imports and returns of methods

--- src/main/java/br/com/fiap/roadassist/repo/IWinchDriverRepo.java
+++ src/main/java/br/com/fiap/roadassist/repo/IWinchDriverRepo.java
@@ -5,7 +5,6 @@ import java.util.UUID;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import br.com.fiap.roadassist.model.WinchDriverModel;
-import java.util.List;
 
 
 public interface IWinchDriverRepo extends JpaRepository<WinchDriverModel, UUID>{
