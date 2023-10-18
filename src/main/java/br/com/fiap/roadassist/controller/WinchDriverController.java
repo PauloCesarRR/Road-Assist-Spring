dcd6ca0ec2cf812e9dde40f9e56b13790a5ed1bd
PauloCesarRR <paulocesarrrpontes@gmail.com>
2023-10-17 21:14:21 -0300

    feat: add try catch in POST WinchDriver

--- src/main/java/br/com/fiap/roadassist/controller/WinchDriverController.java
+++ src/main/java/br/com/fiap/roadassist/controller/WinchDriverController.java
@@ -4,9 +4,11 @@ import org.apache.tomcat.util.json.JSONParser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.fiap.roadassist.errors.ExceptionHandlerController;
 import br.com.fiap.roadassist.model.WinchDriverModel;
 import br.com.fiap.roadassist.repo.IWinchDriverRepo;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -24,23 +26,21 @@ public class WinchDriverController {
 
     @PostMapping("/")
     public ResponseEntity create(@RequestBody WinchDriverModel winchDriverModel){
-    try {
-        
-    } catch (Exception e) {
-        // TODO: handle exception
-    }
+        try {
+            var winchDriver = this.repo.findByEmail(winchDriverModel.getEmail());
 
-        var winchDriver = this.repo.findByEmail(winchDriverModel.getEmail());
+            if(winchDriver != null){
+                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Este Motorista de Caminhão já existe");
+            }
 
-        if(winchDriver != null){
-            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Este Motorista de Caminhão já existe");
+            var passwordHashred = BCrypt.withDefaults().hashToString(12,winchDriverModel.getPassword().toCharArray());
+
+            winchDriverModel.setPassword(passwordHashred);
+
+            var winchDriverCreated = this.repo.save(winchDriverModel);
+            return ResponseEntity.status(HttpStatus.CREATED).body(winchDriverCreated);
+        } catch (HttpMessageNotReadableException e) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMostSpecificCause().getMessage());
         }
-
-        var passwordHashred = BCrypt.withDefaults().hashToString(12,winchDriverModel.getPassword().toCharArray());
-
-        winchDriverModel.setPassword(passwordHashred);
-
-        var winchDriverCreated = this.repo.save(winchDriverModel);
-        return ResponseEntity.status(HttpStatus.CREATED).body(winchDriverCreated);
     }
 }
