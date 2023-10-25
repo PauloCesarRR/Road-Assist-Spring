6acd5da112412703bc833a7917d80b1e031f54d2
PauloCesarRR <pribeirorpontes@gmail.com>
2023-10-24 21:35:41 -0300

    fix: error in City and Compatibility Routes fixed :bug:

--- src/main/java/br/com/fiap/roadassist/repo/ICityRepo.java
+++ src/main/java/br/com/fiap/roadassist/repo/ICityRepo.java
@@ -8,8 +8,6 @@ import org.springframework.data.jpa.repository.Query;
 import br.com.fiap.roadassist.model.CityModel;
 
 public interface ICityRepo extends JpaRepository<CityModel, UUID>{
-    @Query(
-        value = "SELECT C FROM tbl_city C WHERE C.name_city = ?1 AND C.state_id =?2, nativeQuery = true"
-    )
+    @Query(value = "SELECT C FROM tbl_city C WHERE C.name_city = ?1 AND C.state_id =?2", nativeQuery = true)
     CityModel findByNameAndState(String name, UUID stateId);
 }
--- src/main/java/br/com/fiap/roadassist/repo/ICompatibilityVehicleWinchRepo.java
+++ src/main/java/br/com/fiap/roadassist/repo/ICompatibilityVehicleWinchRepo.java
@@ -8,8 +8,6 @@ import org.springframework.data.jpa.repository.Query;
 import br.com.fiap.roadassist.model.CompatibilityVehicleWinchModel;
 
 public interface ICompatibilityVehicleWinchRepo extends JpaRepository<CompatibilityVehicleWinchModel,UUID>{
-    @Query(
-        value = "SELECT * FROM tbl_compatibility_vehicle_winch C WHERE C.type_winch_id = ?1 AND C.type_vehicle_id = ?2, nativeQuery = true"
-    )
+    @Query(value = "SELECT * FROM tbl_compatibility_vehicle_winch C WHERE C.type_winch_id = ?1 AND C.type_vehicle_id = ?2", nativeQuery = true)
     CompatibilityVehicleWinchModel findByTypeWinchAndTypeVehicle(UUID typeWinchId, UUID typeVehicleId);
 }
